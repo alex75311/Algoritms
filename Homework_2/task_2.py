3f1052c7fa1a80e30b8fd660b620ab217c5d8a6a
Isaev Aleksey <isaev.alex75311@gmail.com>
2020-02-18 01:35:23 +0300

    task_2

--- Homework_2/task_2.py
+++ Homework_2/task_2.py
@@ -1,3 +1,8 @@
+# https://drive.google.com/file/d/1aDoOlVjwWrMfFt_Xk38XNJsqyyiZR1MD/view?usp=sharing
+# Посчитать четные и нечетные цифры введенного натурального числа. Например, если введено число 34560, то у него 3
+# четные цифры (4, 6 и 0) и 2 нечетные (3 и 5).
+
+
 cht = ncht = 0
 num = int(input('Введите натуральное число '))
 while num % 10 != 0 or num // 10 != 0:
