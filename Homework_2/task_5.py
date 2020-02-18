04df3ebbb6b51ea201ef698afc1f280888bac224
Isaev Aleksey <isaev.alex75311@gmail.com>
2020-02-18 04:18:04 +0300

    task_5

---
+++ Homework_2/task_5.py
@@ -0,0 +1,16 @@
+# https://drive.google.com/file/d/1aDoOlVjwWrMfFt_Xk38XNJsqyyiZR1MD/view?usp=sharing
+# Вывести на экран коды и символы таблицы ASCII, начиная с символа под номером 32 и заканчивая 127-м включительно.
+# Вывод выполнить в табличной форме: по десять пар "код-символ" в каждой строке.
+
+
+x = 32
+while x < 127:
+    k = 0
+    while k < 10:
+        print(f'{x}={chr(x)} ', end='')
+        k += 1
+        x += 1
+        if x > 127:
+            break
+    k = 0
+    print('\r')
