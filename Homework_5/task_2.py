15b7d99b42a05b6dcb3e5d33f617ab371727955a
Isaev Aleksey <isaev.alex75311@gmail.com>
2020-03-18 06:19:05 +0300

    Доработака замечаний

--- Homework_5/task_2.py
+++ Homework_5/task_2.py
@@ -8,37 +8,44 @@
 
 import collections
 
-hex_list = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
+BASE = 16
 
-num1 = input('Введите первое число ').upper()
-num2 = input('Введите второе число ').upper()
+HEX_LIST = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
 
+BIN_NUMBERS = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5,
+               '6': 6, '7': 7, '8': 8, '9': 9, 'A': 10, 'B': 11,
+               'C': 12, 'D': 13, 'E': 14, 'F': 15}
 
-def check_hex_num(spam):
-    mem = spam // 16
-    spam %= 16
-    return spam, mem
+num1 = collections.deque(input('Введите первое число ').upper())
+num2 = collections.deque(input('Введите второе число ').upper())
 
 
 def hex_sum(num1, num2):
+    # Делаем копию объектов, чтоб не сломать их и использовать ниже по коду
+    num1 = num1.copy()
+    num2 = num2.copy()
     mem = 0
     num1 = collections.deque(num1)
     num2 = collections.deque(num2)
     if len(num1) < len(num2):
         num1, num2 = num2, num1
-    num1.reverse()
-    num2.reverse()
+    num2.extendleft('0' * (len(num1) - len(num2)))
     res = collections.deque()
-    for i in range(len(num1)):
-        if i < len(num2):
-            spam = hex_list.index(num1[i]) + hex_list.index(num2[i]) + mem
+    while len(num1) != 0:
+        first_num = BIN_NUMBERS[num1.pop()]
+        second_num = BIN_NUMBERS[num2.pop()]
+        result_num = first_num + second_num + mem
+        if result_num >= BASE:
+            mem = 1
+            result_num -= BASE
         else:
-            spam = hex_list.index(num1[i]) + mem
-        spam, mem = check_hex_num(spam)
-        res.appendleft(hex_list[spam])
+            mem = 0
+        res.appendleft(HEX_LIST[result_num])
+
     if mem == 1:
         res.appendleft('1')
-    return ''.join(list(res))
+
+    return ''.join(res)
 
 
 def hex_multiplication(num1, num2):
@@ -50,13 +57,38 @@ def hex_multiplication(num1, num2):
         num1, num2 = num2, num1
     num1.reverse()
     for i in range(len(num1)):
-        multiplier += hex_list.index(num1[i]) * 16 ** i
+        multiplier += HEX_LIST.index(num1[i]) * 16 ** i
     for _ in range(multiplier):
-        res = hex_sum(res, num2)
+        res = hex_sum(collections.deque(res), collections.deque(num2))
     if not res:
         return 0
     return res
 
 
+# Вариант преподавателя, немного переделанный под мою функцию hex_sum
+def mult_hex(first, second):
+    first = first.copy()
+    second = second.copy()
+
+    if len(second) > len(first):
+        first, second = second, first
+
+    second.extendleft('0' * (len(first) - len(second)))
+    result = collections.deque('0')
+
+    while len(second) != 0:
+        second_num = BIN_NUMBERS[second.pop()]
+
+        spam = collections.deque('0')
+        for _ in range(second_num):
+            spam = collections.deque(hex_sum(collections.deque(spam), collections.deque(first)))
+
+        spam.extend('0' * (len(first) - len(second) - 1))
+        result = hex_sum(collections.deque(result), collections.deque(spam))
+
+    return result
+
+
 print(f'Сумма равна {hex_sum(num1, num2)}')
 print(f'Произведение равно {hex_multiplication(num1, num2)}')
+print(f'Произведение равно {mult_hex(num1, num2)}')
